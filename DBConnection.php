7bb28beec2bf2f245fdd75725ae39e37f1f22d5b
janakalidasan T <janakalidasant@gmail.com>
2023-08-22 15:22:57 +0530

    live database link added

--- DBConnection.php
+++ DBConnection.php
@@ -1,6 +1,7 @@
 <?php
   $conn = mysqli_connect('localhost','root','','leaveform');
-  
+  // live
+  //  $conn = mysqli_connect('sql307.infinityfree.com','if0_34870563','E8lXs09mlNtpKt','if0_34870563_leaveform');
   if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error."<br>");
   }
